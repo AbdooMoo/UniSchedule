c8be18d4a4b876357f382cbc3985cba254dcc177
Youssef <yousef1289yy@gmail.com>
2026-03-03 21:58:30 +0200

    Create student dashboard and style in src/pages and src/styles

---
+++ front_end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentDashboard from './pages/S_Dashboard'; // بننادي على صفحتك
+import './styles/S_style.css'; // بننادي على استايلاتك
+
+function App() {
+  return (
+    <div className="App">
+      <StudentDashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ front_end/src/pages/S_Dashboard.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import {FaHome,FaUser,FaCalendarAlt,FaSignOutAlt} from 'react-icons/fa';
+import '../styles/S_style.css';
+import logo from '../assets/logo.jpeg'; 
+
+
+const S_Dashboard = () => {
+    //
+    const [isRegistrationOpen] = useState(false);
+
+    return (
+        <div className="dashboard-container">
+            
+           {/*1*/}
+            <div className="sidebar">
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginBottom: '30px' }}>
+                  
+                    <img src={logo} alt="Logo" style={{ height: '30px', marginRight: '10px' }} />
+                    <h2 style={{ margin: 0 }}>Student Portal</h2>
+                </div>
+                <nav>
+                    <button title="Go to Home Page">
+                        <FaHome className="icon"/>Home</button>
+                    <button style={{ opacity: 0.5 }} title="Viev Profile">
+                        <FaUser className="icon"/>Profile</button>
+                    <button style={{ opacity: 0.5 }} title="Viev your class schedules">
+                        <FaCalendarAlt className="icon"/>My Schedule</button>
+                    <button style={{ marginTop: 'auto', color: '#d74141' }}>
+                        <FaSignOutAlt className="icon"/>Logout
+                    </button>
+                </nav>
+            </div>
+
+            {/*2*/}
+            <div className="main-content">
+                
+                <div className="background-overlay"></div>
+                
+                
+                
+                    <header className="header">
+                        <div className="welcome-msg">
+                            <h1>Welcome back</h1>
+                        </div>
+                    </header>
+
+                    {isRegistrationOpen ?(<div className="schedule-table">
+                        <h2>Yoyr weekly Schedule</h2>
+                    </div>
+
+                    ):
+                    (
+                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexGrow:1, paddingBottom:'50px' }}>
+                            <div className="schedule-card">
+                                <span className="status-icon">⏳</span>
+                                <h3>Schedules Not Published</h3>
+                                <p>
+                                    The registration period is currently closed. <br />
+                                    Please check back later when the administration publishes the finalized timetables.
+                                </p>
+                                <div className="status-badge" >
+                                    Status: Preparation Mode
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                
+            </div>
+        </div>
+    );
+};
+
+export default S_Dashboard;
